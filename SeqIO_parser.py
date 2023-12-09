c48e42955f31a682ec53eebd691a5daeacf79256
MoSafi2 <mohamed.mabrouk@rwth-aachen.de>
2023-12-09 16:06:07 +0100

    minor changes

--- SeqIO_parser.py
+++ SeqIO_parser.py
@@ -2,12 +2,16 @@ from Bio import SeqIO
 from time import time_ns
 
 count = 0
+bases = 0
 t1 = time_ns()
-for record in SeqIO.parse("data/SRR4381936.fastq", "fastq"):
+for record in SeqIO.parse("data/9_Swamp_S2B_rbcLa_2019_minq7.fastq", "fastq"):
     list(record)
     count = count + 1
+    bases = bases + len(record)
+
 
 t2 = time_ns()
 
-print((t2-t1)/1e9, " Seconds")
-print(count, " recods")
+print((t2-t1)/1e9, "Seconds")
+print(count, "records")
+print(bases, "total bases")
